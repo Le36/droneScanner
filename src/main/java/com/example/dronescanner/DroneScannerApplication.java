4a361513400a8a093b8cbefb02f8a75e02569621
Le36 <48809753+Le36@users.noreply.github.com>
2022-12-30 11:54:49 +0200

    project init

---
+++ src/main/java/com/example/dronescanner/DroneScannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dronescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneScannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dronescanner/DroneScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dronescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
