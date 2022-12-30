371b9bd4a2ca65283c756f7b4307a99c33785564
Le36 <48809753+Le36@users.noreply.github.com>
2022-12-30 12:08:13 +0200

    mvp

--- src/main/java/com/example/dronescanner/DroneScannerApplication.java
+++ src/main/java/com/example/dronescanner/DroneScannerApplication.java
@@ -2,12 +2,43 @@ package com.example.dronescanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
+@Controller
 public class DroneScannerApplication {
+
+    private final ScannerMain scannerMain = new ScannerMain();
 
     public static void main(String[] args) {
         SpringApplication.run(DroneScannerApplication.class, args);
     }
 
+    @Scheduled(fixedRate = 2000)
+    public void listen() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject("https://assignments.reaktor.com/birdnest/drones", String.class);
+        scannerMain.handle(response);
+    }
+
+    @GetMapping("/violations")
+    @ResponseBody
+    public List<String> getResponses() {
+        return scannerMain.getViolations();
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("data", scannerMain.getViolations());
+        return "home";
+    }
 }
